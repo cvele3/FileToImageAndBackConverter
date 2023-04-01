df6542ff9c745fabfaf11f1e72379f371c72f661
Filip Paris <parisfilip2@gmail.com>
2023-04-01 21:54:59 +0200

    additional changes for converting images to video and back

--- src/main/java/ImagesToVideo/ImageToVideo.java
+++ src/main/java/ImagesToVideo/ImageToVideo.java
@@ -1,22 +1,24 @@
 package ImagesToVideo;
 
-import java.io.*;
+import org.apache.commons.io.FileUtils;
 
-import static java.lang.Thread.sleep;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ImageToVideo {
 
-    public static void convertToVideoAndBack() throws IOException, InterruptedException {
+    public static void convertToVideoAndBack(String rootPath, int totalPixels) throws IOException, InterruptedException {
 
-        // Input images directory
-        String imagesDir = "C:/Users/paris/OneDrive/Dokumenti/convertfile/slike/image%03d.png";
-        String imagesDir2 = "C:/Users/paris/OneDrive/Dokumenti/convertfile/slikeVidea/image%03d.png";
 
-        // Output video file
-        String videoFile = "C:/Users/paris/OneDrive/Dokumenti/convertfile/test.mp4";
+        File pathToVideo = new File(rootPath + "video");
+        FileUtils.cleanDirectory(pathToVideo);
 
-        File file = new File(videoFile);
-        if (file.exists()) file.delete();
+        String videoFile = rootPath + "video/"+totalPixels+".mp4";
 
         // Frame rate of the output video
         int frameRate = 10;
@@ -25,7 +27,7 @@ public class ImageToVideo {
         ProcessBuilder builder = new ProcessBuilder(
                 "cmd.exe",
                 "/c",
-                "ffmpeg -r " + frameRate + " -f image2 -s 1920x1080 -i " + imagesDir + " -vcodec libx264 -crf 25 -pix_fmt yuv420p " + videoFile);
+                "ffmpeg -r " + frameRate + " -f image2 -s 1920x1080 -i " + rootPath + "slike/image%03d.png" + " -vcodec libx264 -crf 25 -pix_fmt yuv420p " + videoFile);
         builder.redirectErrorStream(true);
         Process p = builder.start();
         BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
@@ -40,12 +42,22 @@ public class ImageToVideo {
             System.err.println("Error: ffmpeg exited with code " + exitCode);
             System.exit(exitCode);
         }
+
+        //Delete original images
+        File imageOriginalFile = new File(rootPath + "slike");
+        FileUtils.deleteDirectory(imageOriginalFile);
+
         // Convert video to images
+
+        Path pathToImagesAfterConversion = Paths.get(rootPath + "slikeVidea");
+        if (!Files.exists(pathToImagesAfterConversion)) {
+            Files.createDirectory(pathToImagesAfterConversion);
+        }
 
         ProcessBuilder videoToImage = new ProcessBuilder(
                 "cmd.exe",
                 "/c",
-                "ffmpeg -i "+ videoFile + " " + imagesDir2);
+                "ffmpeg -i "+ videoFile + " " + pathToImagesAfterConversion + "/image%03d.png");
         builder.redirectErrorStream(true);
         Process process = videoToImage.start();
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
